8e8d2042691ff493b12b264cd1eac7e2539d1e40
Vladimir Panteleev <vladimir@thecybershadow.net>
2012-04-10 01:50:44 +0300

    disassembler: Relax filename sanitization for names like "controller"

--- disassembler.d
+++ disassembler.d
@@ -660,7 +660,7 @@ final class RefBuilder : ASTraitsVisitor
 				static const reservedNames = ["CON", "PRN", "AUX", "NUL", "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9"];
 				auto pathSegmentU = pathSegment.toUpper();
 				foreach (reservedName; reservedNames)
-					if (pathSegmentU.startsWith(reservedName))
+					if (pathSegmentU.startsWith(reservedName) && (pathSegmentU.length == reservedName.length || pathSegmentU[reservedName.length]=='.'))
 					{
 						pathSegment = "%" ~ pathSegment;
 						break;
